91cf189d674340a0846a50118a16609f6739ecb6
Miliquid <miliquid104@gmail.com>
2018-06-16 12:37:43 +0300

    DSas
    Optimizated

--- Excell reader/Logic.cs
+++ Excell reader/Logic.cs
@@ -8,114 +8,101 @@ using System.Threading.Tasks;
 using Application = Microsoft.Office.Interop.Excel;
 namespace Excell_reader
 {
-    class Logic
+	class Logic
+	{
+		private const string DisciplineColumn = "E";
+		private const string SemesterColumn = "F";
+		private const string TeacherColumn = "P";
+		private const string TotalHoursColumn = "S";
+		private const long StartingRow = 6;
 
-    {
-        
-        public void read(string FirstFile, string TeacherFio, double SemestrInsert)
-        {
+		private Application.Application app;
+		private Application.Workbooks wrbks;
+		private Application.Workbook wrbk;
+		private Application.Worksheet wrsh;
 
+		public void read(string FirstFile, string TeacherFio, double SemestrInsert)
+		{
+			double totalHours = 0;
 
-            Application.Application app;
-            Application.Workbooks wrbks = null;
-            Application.Workbook wrbk = null;
-            Application.Worksheet wrsh;
-            
-            double workTime = 0;
+			app = new Application.Application { DisplayAlerts = true };
+			try
+			{
+				wrbks = app.Workbooks;
+				wrbk = wrbks.Open(Path.Combine(Environment.CurrentDirectory, FirstFile));
 
+				long SheetCount = wrbk.Worksheets.Count;
+				long CurrentSheetIndex = 1;
+				long RowIndex = StartingRow;
 
-            app = new Application.Application { DisplayAlerts = true };
-            try
-            {
+				bool finished = false;
 
+				do
+				{
+					wrsh = wrbk.Worksheets[CurrentSheetIndex++];
 
+					while (!finished)
+					{
+						object end = wrsh.Range[String.Format("A{0}", RowIndex)].Value;
+						if (end == null)
+							finished = true;
 
+						string TeacherExcell = wrsh.Range[String.Format("{0}{1}", TeacherColumn, RowIndex)].Value;
+						if (TeacherExcell != null)
+						{
+							TeacherExcell = TeacherExcell.Remove(0, 1);
+							TeacherExcell = TeacherExcell.Replace(" ", "").Replace(".", "");
+							TeacherFio = TeacherFio.Replace(" ", "").Replace(".", "");
 
+						}
 
-                wrbks = app.Workbooks;
-                wrbk = wrbks.Open(Path.Combine(Environment.CurrentDirectory, FirstFile));
+						if (TeacherFio == TeacherExcell)
+						{
+							double SemestrTwo = wrsh.Range[String.Format("{0}{1}", SemesterColumn, RowIndex)].Value;
+							if (SemestrInsert == SemestrTwo)
+							{
+								string Discpline1 = wrsh.Range[String.Format("{0}{1}", DisciplineColumn, RowIndex)].Value;
+								Discpline1 = Discpline1.Replace(" ", "").Replace("(Экзаменатор)", "");
+								string Discpline2 = wrsh.Range[String.Format("{0}{1}", DisciplineColumn, RowIndex - 1)].Value;
 
-                long ShCount = wrbk.Worksheets.Count;
-                long SheetCount = 1;
-                long NumSecond = 5;
-                bool cil = true;
+								if (Discpline2 != null)
+								{
 
+									Discpline2 = Discpline2.Replace(" ", "").Replace("(Экзаменатор)", "");
+								}
+								if ((Discpline1 == Discpline2) || (Discpline2 == null))
+								{
+									totalHours += wrsh.Range[String.Format("{0}{1}", TotalHoursColumn, RowIndex)].Value;
+									//System.Windows.MessageBox.Show("+");
+								}
 
+								//   double end = wrsh.Range[String.Format("A{0}", RowIndex)].Value;
+								// if (end == null)
+								//     Finish = false;
 
-                do
-                {
+							}
+						}
+						RowIndex++;
 
+					}
+					Marshal.ReleaseComObject(wrsh);
 
-                    wrsh = wrbk.Worksheets[SheetCount++];
-                    do
-                    {
-                        NumSecond++;        //листаем строки
-                        string TeacherExcell = wrsh.Range["P" + NumSecond].Value;
-                        if (TeacherExcell != null)
-                        {
-                            TeacherExcell = TeacherExcell.Remove(0, 1);
-                            TeacherExcell = TeacherExcell.Replace(" ", "").Replace(".", "");
-                            TeacherFio = TeacherFio.Replace(" ", "").Replace(".", "");
+				} while (CurrentSheetIndex <= SheetCount);
+			}
+			catch (Exception E)
+			{
+				System.Windows.MessageBox.Show("Eror" + E.Message);
+			}
+			finally
+			{
+				app.Workbooks.Close();
+				app.Quit();
 
-                        }
+				Marshal.ReleaseComObject(wrbk);
+				Marshal.ReleaseComObject(wrbks);
 
-                        if (TeacherFio == TeacherExcell)
-                        {
-                            // System.Windows.MessageBox.Show("Гыыы");
-
-
-                            double SemestrTwo = wrsh.Range["F" + NumSecond].Value;
-                            if (SemestrInsert == SemestrTwo)
-                            {
-                                string Discpline1 = wrsh.Range["E" + NumSecond].Value;
-                                Discpline1 = Discpline1.Replace(" ", "").Replace("(Экзаменатор)", "");
-                                string Discpline2 = wrsh.Range["E" + (NumSecond - 1)].Value;
-
-                                if (Discpline2 != null)
-                                {
-
-                                    Discpline2 =Discpline2.Replace(" ", "").Replace("(Экзаменатор)", "");
-                                }
-                                if ((Discpline1 == Discpline2) || (Discpline2 == null))
-                                {
-                                    workTime += wrsh.Range["S" + (NumSecond)].Value;
-                                    System.Windows.MessageBox.Show("+");
-                                }
-                                
-                                
-                                //cil = false;
-                                // System.Windows.MessageBox.Show("Гыыы");
-                            }
-
-                        }
-                        //else NumSecond += 1,kll;
-                    } while (cil);
-                    Marshal.ReleaseComObject(wrsh);
-                } while (SheetCount <= ShCount);
-
-
-
-
-            }
-            catch(Exception E)
-            {
-                System.Windows.MessageBox.Show("Eror"+E.Message);
-
-
-            }
-            finally
-            {
-                app.Workbooks.Close();
-                app.Quit();
-                
-                
-                Marshal.ReleaseComObject(wrbk);
-                Marshal.ReleaseComObject(wrbks);
-
-                System.Windows.MessageBox.Show(workTime.ToString());
-
-
-            }
+				System.Windows.MessageBox.Show(totalHours.ToString());
+			}
         }
     }
 }
--- Excell reader/MainWindow.xaml.cs
+++ Excell reader/MainWindow.xaml.cs
@@ -66,7 +66,7 @@ namespace Excell_reader
         {app = new Application.Application { DisplayAlerts = true };
             var Lo = new Logic();
             
-            Lo.read(FFile.Text, Fio.Text, semestr.Value);
+            Lo.read(FFile.Text, Fio.Text, semestr.SelectedIndex);
 
         }
 
